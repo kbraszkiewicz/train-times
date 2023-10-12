a7f8f85d490901aabdccaf4db4a5239ea0e815fd
kevin b <kevinb230402@gmail.com>
2023-10-12 10:42:28 +0100

    Render page in flask route

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return render_template("template.html")
 
 
 @app.route("/login")
 def login():
     return "Login Page!"
+
+
+# if __name__ == "__main__":
+
+#     app.run(host="0.0.0.0")
