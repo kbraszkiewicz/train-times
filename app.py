97ff296ea318400190d35b3768f822ec1777b332
kevin b <kevinb230402@gmail.com>
2023-10-12 12:00:00 +0100

    simple login form plus csrf exemption

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 from flask import Flask
 from flask import render_template
 from dataclasses import dataclass
+from flask import request
+
+from forms import Login
+
+from flask import Flask, render_template, redirect, url_for
+# from flask_bootstrap import Bootstrap5
+
+from flask_wtf.csrf import CSRFProtect
+
+
+
+
 
 
 app = Flask(__name__, static_folder='static')
+csrf = CSRFProtect(app)
+
+
+# Bootstrap-Flask requires this line
+# bootstrap = Bootstrap5(app)
+# Flask-WTF requires this line
+
 
 @dataclass
 class Train:
@@ -25,9 +44,14 @@ def hello():
     return render_template("template.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=['POST', 'GET'])
+@csrf.exempt
 def login():
-    return "Login Page!"
+    if request.method == 'POST':
+            return redirect(url_for('hello'))
+    return render_template('login.html')
+
+
 
 
 # if __name__ == "__main__":
